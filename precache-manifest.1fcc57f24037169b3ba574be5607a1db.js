8c376db93c813e9ff6ede9eed4a8341e700c1ee6
gigigimay <mo.maytiya_st@tni.ac.th>
2019-10-27 16:14:46 +0700

    Updates

---
+++ precache-manifest.1fcc57f24037169b3ba574be5607a1db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1323f0f4552ae6096c3c276f33b84d89",
+    "url": "/shim-shop-chai/index.html"
+  },
+  {
+    "revision": "b4941965113c5031ab46",
+    "url": "/shim-shop-chai/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "e67b2afc74fb31990840",
+    "url": "/shim-shop-chai/static/css/main.c718d646.chunk.css"
+  },
+  {
+    "revision": "b4941965113c5031ab46",
+    "url": "/shim-shop-chai/static/js/2.58c9642c.chunk.js"
+  },
+  {
+    "revision": "e67b2afc74fb31990840",
+    "url": "/shim-shop-chai/static/js/main.c9cf9449.chunk.js"
+  },
+  {
+    "revision": "bd8ebe52fa05236d7cb2",
+    "url": "/shim-shop-chai/static/js/runtime-main.edcb3d35.js"
+  }
+]);
